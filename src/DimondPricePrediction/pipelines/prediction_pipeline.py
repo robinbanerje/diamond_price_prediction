dcc0b8f695927117a1f00ea0e518735c009d5901
Robin Banerjee <robinbanerje987@gmail.com>
2023-11-04 09:52:10 +0530

    predict function in prediction pipeline

--- src/DimondPricePrediction/pipelines/prediction_pipeline.py
+++ src/DimondPricePrediction/pipelines/prediction_pipeline.py
@@ -9,5 +9,19 @@ class PredictPipeline:
     def __init__(self):
         pass
 
-    def predict(self):
-        pass
+    def predict(self, features):
+        try:
+            preprocessor_path = os.path.join("artifacts", "preprocessor.pkl")
+            model_path = os.path.join("artifacts", "model.pkl")
+            
+            preprocessor = load_object(preprocessor_path)
+            model = load_object(model_path)
+
+            scaled_data = preprocessor.transform(features)
+            
+            pred = model.predict(scaled_data)
+
+            return pred
+
+        except Exception as e:
+            raise customexception(e, sys)
