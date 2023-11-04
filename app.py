ed82843efff3e86a0b6669450e757c4d11d468f8
Robin Banerjee <robinbanerje987@gmail.com>
2023-11-04 10:22:09 +0530

    app.py update

---
+++ app.py
@@ -0,0 +1,9 @@
+from src.DimondPricePrediction.pipelines.prediction_pipeline import CustomData, PredictPipeline
+
+from flask import Flask, request, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
